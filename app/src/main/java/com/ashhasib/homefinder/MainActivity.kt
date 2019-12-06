2649d54ca88f6789d1eef97c973b7e02d0ed4b7f
AshHasib <ashhasib54@gmail.com>
2019-12-06 20:15:11 +0600

    Retrofit added and tested against localhost successfully

--- app/src/main/java/com/ashhasib/homefinder/MainActivity.kt
+++ app/src/main/java/com/ashhasib/homefinder/MainActivity.kt
@@ -3,6 +3,15 @@ package com.ashhasib.homefinder
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.ashhasib.homefinder.api_interface.ApiClient
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +19,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, LoginActivity::class.java))
+        //startActivity(Intent(this, LoginActivity::class.java))
+
+        val retrofit = Retrofit.Builder().baseUrl("http://10.0.2.2:8000/api/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiClient = retrofit.create(ApiClient::class.java)
+
+        val call = apiClient.helloMessage
+
+        call.enqueue(object :Callback<String>{
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.d("RETROFIT", t.stackTrace.toString())
+                Log.d("RETROFIT", t.message)
+                Log.d("RETROFIT", "FAIL")
+
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+
+                if( response.isSuccessful ){
+                    txtMessage.setText(response.body())
+                    Log.d("RETROFIT", response.message())
+                    Log.d("RETROFIT", "SUCCESS")
+                }
+            }
+
+        })
+
+
+
     }
 }
